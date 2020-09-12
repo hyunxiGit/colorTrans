89e927f7320f1f66f516de33201bb32510a83b50
hyunxi <hyunxi@hotmail.com>
2020-09-11 17:40:00 -0700

    add matrix add read write img

---
+++ colorTrans.py
@@ -0,0 +1,95 @@
+from PIL import Image ,ImageFont,ImageDraw
+import numpy as np
+
+# path
+path_r = 'img/sky_s0.png'
+path_s = 'img/sky_s1.png'
+path_t = 'img/temp.png'
+
+def arrayFromImg(_path = path_r):
+    _img = Image.open(_path)
+    _px = _img.load()
+    _colorArray = np.ndarray(shape=(_img.width, _img.height, 3), dtype=np.float, order='C')
+    for y in range(_img.height):
+        for x in range(_img.width):
+            _colorArray[x, y] = _px[x, y]
+    return _colorArray
+
+def imgFromArray(_colorArray,_path = path_t):
+    _img = Image.new("RGB", (_colorArray.shape[0],_colorArray.shape[1]), (0,0,0))
+    _px = _img.load()
+    for y in range(_img.height):
+        for x in range(_img.width):
+            _px[x, y] = tuple(_colorArray[x,y])
+
+    _img.save(_path)
+
+def convert_int_float(_i):
+    _f=(_i[0]/255,_i[1]/255,_i[2]/255)
+    return (_f)
+
+s_img_array = arrayFromImg(path_s)
+r_img_array = arrayFromImg(path_r)
+
+for y in range(s_img_array.shape[1]):
+    for x in range(s_img_array.shape[0]):
+        _rgb = np.array(convert_int_float(s_img_array[x, y]))
+
+
+
+# # exaple array - correct way of multiply matrix and matrix vector
+# xyz_lms = np.array([[0.3897, 0.689,-0.0787],[-0.2298,1.1834,0.0464],[0,0,1]])
+# rbg_xyz = np.array([[0.5141,0.3239,0.1604],[0.2651,0.6702,0.0641],[0.0241,0.1228,0.8444]])
+# c = np.array([1,1,1])
+# rgb_lms = xyz_lms@rbg_xyz
+# c = xyz_lms@c
+#
+# print(rgb_lms)
+# print(c)
+# # exaple array - correct way of multiply matrix and matrix vector
+
+m_rgb_lms = np.array([[0.3811, 0.5783,0.0402],
+                      [0.1967,0.7244,0.0782],
+                      [0.0241,0.1228,0.8444]])
+
+m_lms_rgb = np.array([[4.4679, -3.5873,0.1193],
+                      [-1.2186,2.3809,-0.1624],
+                      [0.0497,-0.2439,1.2045]])
+
+
+m_lms_lab_0 = np.array([[1/np.sqrt(3),0,0],
+                        [0,1/np.sqrt(6),0],
+                        [0,0,1/np.sqrt(2)]])
+m_lms_lab_1 = np.array([[1,1,1],
+                        [1,1,-2],
+                        [1,-1,0]])
+m_lms_lab = m_lms_lab_0 @m_lms_lab_1
+
+m_lab_lms_0 = np.array([[1,1,1],
+                        [1,1,-1],
+                        [1,-2,0]])
+m_lab_lms_1 = np.array([[np.sqrt(3)/3,0,0],
+                        [0,np.sqrt(6)/6,0],
+                        [0,0,np.sqrt(2)/2]])
+m_lab_lms = m_lab_lms_0 @ m_lab_lms_1
+
+def convert_rgb_lab (_rgb):
+    _lms = m_rgb_lms@_rgb
+    # log 10
+    _lms[0] = np.log10(_lms[0])
+    _lms[1] = np.log10(_lms[1])
+    _lms[2] = np.log10(_lms[2])
+    _lab = m_lms_lab@_lms
+    return (_lab)
+
+def convert_lab_rgb(_lab):
+    _lms = m_lab_lms@_lab
+    # power 10
+    _lms[0] = np.power( 10,_lms[0])
+    _lms[1] = np.power( 10,_lms[1])
+    _lms[2] = np.power( 10,_lms[2])
+    # rgb
+    _rgb = m_lms_rgb@_lms
+    return(_rgb)
+
+arrayFromImg()
